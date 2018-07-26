2209a27527d2af50de8d3420b84ab4ec0a54c5fc
janggeguk <jkjang81@wonderpeople.co.kr>
2018-07-26 09:42:40 +0900

    header commit

--- front-end/src/components/base/header/Header.tsx
+++ front-end/src/components/base/header/Header.tsx
@@ -1,7 +1,5 @@
-
 import * as React from 'react';
 import i18n from 'i18n';
-import './Header.scss';
 
 interface HeaderProps {
   onOpen(): void;
@@ -9,45 +7,50 @@ interface HeaderProps {
 
 const Header: React.SFC<HeaderProps> = (props, t) => {
   return (
-  <header className="header py-3">
-  <div className="row">
-    <div className="flex-3 d-flex">
-      {/* <a className="text-muted" href="#">
+    <header className="header py-3">
+      <div className="row">
+        <div className="flex-3 d-flex">
+          {/* <a className="text-muted" href="#">
 
       </a> */}
-    </div>
-    <div className="flex-3 text-center">
-      <a className="header-logo text-dark" href="#">
-      {i18n.t('age.label')}
-      </a>
-    </div>
-    <div className="flex-3 d-flex justify-content-end align-items-center">
-      <a className="text-muted" href="#">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="search"
-        >
-          <circle cx="10.5" cy="10.5" r="7.5" />
-          <line x1="21" y1="21" x2="15.8" y2="15.8" />
-        </svg>
-      </a>
-      <a className="btn no-outline " href="#" onClick={props.onOpen}>
-        Sign in
-      </a>
-      <a className="btn" href="#">
-        Get Started
-      </a>
-    </div>
-  </div>
-    </header>);
+        </div>
+        <div className="flex-3 text-center">
+          <a className="header-logo text-dark" href="#">
+            {i18n.t('serviceName.label')}
+          </a>
+        </div>
+        <div className="flex-3 d-flex justify-content-end align-items-center">
+          <a className="text-muted" href="#">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="search"
+            >
+              <circle cx="10.5" cy="10.5" r="7.5" />
+              <line x1="21" y1="21" x2="15.8" y2="15.8" />
+            </svg>
+          </a>
+          <a className="btn no-outline " href="#" onClick={props.onOpen}>
+            {i18n.t('signInBtnName.label')}
+          </a>
+          <a className="text" href="#">
+            {i18n.t('selectLangBtnName.label')}
+          </a>
+          <span className="divide">|</span>
+          <a className="btn" href="#">
+            {i18n.t('startBtnName.label')}
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- front-end/src/i18n.js
+++ front-end/src/i18n.js
@@ -1,29 +1,30 @@
 import i18next from 'i18next';
 
-i18next
-  .init({
-    interpolation: {
-      // React already does escaping
-      escapeValue: false,
+i18next.init({
+  interpolation: {
+    // React already does escaping
+    escapeValue: false
+  },
+  lng: 'en', // 'en' | 'es'
+  // Using simple hardcoded resources for simple example
+  resources: {
+    en: {
+      translation: {
+        serviceName: { label: 'Angler Story' },
+        signInBtnName: { label: 'sign in' },
+        startBtnName: { label: 'Get Started' },
+        selectLangBtnName: { label: 'lanuage' }
+      }
     },
-    lng: 'en', // 'en' | 'es'
-    // Using simple hardcoded resources for simple example
-    resources: {
-      en: {
-        translation: {
-          age: { label: 'Age', },
-          home: { label: 'Home', },
-          name: { label: 'Name', },
-        },
-      },
-      es: {
-        translation: {
-          age: { label: 'Años', },
-          home: { label: 'Casa', },
-          name: { label: 'Nombre', },
-        },
-      },
-    },
-  })
+    kr: {
+      translation: {
+        serviceName: { label: '낚시꾼의 이야기' },
+        signInBtnName: { label: '로그인' },
+        startBtnName: { label: '시작하기' },
+        selectLangBtnName: { label: '언어' }
+      }
+    }
+  }
+});
 
-export default i18next
+export default i18next;
